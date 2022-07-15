dde123f934a51123252ac3860f7d1b738b942d1a
Siarhei Sialitski <siarhei.sialitski.ws@gmail.com>
2022-07-15 10:32:32 +0300

    Message based communication implemented

--- src/DotNetMP.Carting.Infrastructure/DependenciesRegistrarExt.cs
+++ src/DotNetMP.Carting.Infrastructure/DependenciesRegistrarExt.cs
@@ -1,7 +1,9 @@
 ﻿using DotNetMP.Carting.Infrastructure.Data;
 using DotNetMP.Carting.Infrastructure.Data.Options;
+using DotNetMP.Carting.Infrastructure.IntegrationEvents;
 using DotNetMP.SharedKernel.Interfaces;
 using LiteDB;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,6 +18,13 @@ public static class DependenciesRegistrarExt
 
         services.AddScoped(typeof(IRepository<>), typeof(LiteDbRepository<>));
         services.AddSingleton<IClientFactory<ILiteDatabase>, LiteDatabaseFactory>();
+
+        services.AddSingleton<IIntegrationEventsSubscriber, ServiceBusIntegrationEventsSubscriber>();
+
+        services.AddAzureClients(builder =>
+        {
+            builder.AddServiceBusClient(configuration.GetConnectionString("ServiceBusConnectionString"));
+        });
 
         return services;
     }
